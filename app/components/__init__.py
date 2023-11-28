5c2de1a21d411b6bb8b56b9b4d73accd93d38d76
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-11-28 18:44:59 +0000

    Lint

--- app/components/__init__.py
+++ app/components/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint(
-    "components", __name__, template_folder="test-templates"
-)
+bp = Blueprint("components", __name__, template_folder="test-templates")
 
 from app.components import routes  # noqa: E402,F401
