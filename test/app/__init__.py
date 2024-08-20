396a9408177c4c0bc6ca35229a745b0e14365051
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-08-20 10:02:35 +0100

    Wait for Docker image to be ready

--- test/app/__init__.py
+++ test/app/__init__.py
@@ -5,6 +5,12 @@ from .templates import bp as templates_bp
 
 app = Flask(__name__, template_folder="/home/app/tna_frontend_jinja/templates")
 
+
+@app.route("/ready/")
+def ready():
+    return "ok"
+
+
 app.register_blueprint(components_bp, url_prefix="/components")
 app.register_blueprint(utilities_bp, url_prefix="/utilities")
 app.register_blueprint(templates_bp, url_prefix="/templates")
