12f6b6f5e3b1832932bcdafff47dd04293e90815
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-06-06 11:07:57 +0100

    Add basic Playwright configuration

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./test/playwright",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 2,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI
+    ? [
+        ["dot"],
+        ["@estruyf/github-actions-reporter"],
+        ["json", { outputFile: "./test/results.json" }],
+      ]
+    : "line",
+  snapshotPathTemplate:
+    "{testDir}/{testFilePath}-snapshots/{arg}-{projectName}{ext}",
+  expect: {
+    toMatchAriaSnapshot: {
+      pathTemplate:
+        "{testDir}/{testFilePath}-snapshots/{arg}-{projectName}-aria{ext}",
+    },
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
