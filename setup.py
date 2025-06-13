9a9be84cca79e58344fc8deb01fd0886761c8c9b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-06-13 16:17:17 +0100

    v0.20.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tna-frontend-jinja",
-    version="0.20.0",
+    version="0.20.1",
     author="Andrew Hosgood",
     author_email="andrew.hosgood@nationalarchives.gov.uk",
     description="TNA Frontend Jinja templates",
