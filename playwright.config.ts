083517e2aae08ca323ff9d4ffebf4e01d5c84277
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-09-19 16:19:22 +0100

    Refactor Playwright reporter configuration to use a consistent line reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,13 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: 2,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI
-    ? [
-        ["dot"],
-        ["@estruyf/github-actions-reporter"],
-        ["json", { outputFile: "./test/results.json" }],
-      ]
-    : "line",
+  reporter: "line",
   use: {
     baseURL: process.env.TEST_DOMAIN || "http://127.0.0.1:5001",
   },
